db262207f4b895d9b78d9fe7e7bd5baf793537db
Felipe Borba <felipe.borba0109@gmail.com>
2021-05-11 11:00:04 -0300

    03-05 add more style and organise import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
-import Form from "./components/Form/Form";
-import NoteList from "./components/NoteList/NoteList";
-
+import React, { Component } from 'react'
+import Form from './components/Form/index'
+import NoteList from './components/NoteList/index'
+import './assets/App.css'
+import './assets/index.css'
 
 export default class App extends Component {
   render() {
     return (
-      <section>
+      <section className='content'>
         <Form />
         <NoteList />
       </section>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,10 +4,20 @@ import './Form.css'
 export default class Form extends Component {
     render() {
         return (
-            <form>
-                <input type='text' placeholder='Title' />
-                <textarea placeholder='write your note...'></textarea>
-                <button>create note</button>
+            <form className='form-register'>
+                <input
+                    type='text'
+                    placeholder='Title'
+                    className='form-register_input'
+                />
+                <textarea
+                    rows={15}
+                    placeholder='write your note...'
+                    className='form-register_input'>
+                </textarea>
+                <button className='form-register_input form-register_submit'>
+                    create note
+                </button>
             </form>
         )
     }
