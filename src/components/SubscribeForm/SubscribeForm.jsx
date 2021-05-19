b063b8638503a66fc03658b8a75066cdce8afa2e
Felipe Borba <felipe.borba0109@gmail.com>
2021-05-19 18:28:06 -0300

    02-02 Add registration form page selector

--- src/components/SubscribeForm/SubscribeForm.jsx
+++ src/components/SubscribeForm/SubscribeForm.jsx
@@ -1,3 +1,4 @@
+import { Typography } from '@material-ui/core';
 import React, { useState } from 'react';
 import Address from './Address';
 import PersonalData from './PersonalData';
@@ -5,13 +6,27 @@ import UserData from './UserData';
 
 
 function SubscribeForm({ submitForm }) {
+    const [currentStep, setCurrentStep] = useState(0);
 
+    function currentForm(currentStep) {
+        switch (currentStep) {
+            case 0:
+                return <UserData />;
+    
+            case 1:
+                return <PersonalData submitForm={submitForm} />;
+    
+            case 2:
+                return <Address />;
+    
+            default:
+                return <Typography>Select form error</Typography>;
+        }
+    }
 
     return (
         <>
-            <PersonalData submitForm={submitForm} />
-            <UserData />
-            <Address />
+            {currentForm(currentStep)}
         </>
     );
 }
