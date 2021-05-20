28773649d5c229bbbacf3a7d22f6ed0ca637f577
Felipe Borba <felipe.borba0109@gmail.com>
2021-05-19 21:12:55 -0300

    03-05 stepper

--- src/components/SubscribeForm/SubscribeForm.jsx
+++ src/components/SubscribeForm/SubscribeForm.jsx
@@ -1,3 +1,4 @@
+import { Step, StepLabel, Stepper, Typography } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import Address from './Address';
 import PersonalData from './PersonalData';
@@ -10,15 +11,18 @@ function SubscribeForm({ submitForm }) {
     const pages = [
         <UserData nextStep={collectData} />,
         <PersonalData nextStep={collectData} />,
-        <Address nextStep={collectData} />
+        <Address nextStep={collectData} />,
+        <Typography variant='h5'>thanks for subscribing</Typography>
     ];
-    
+
     useEffect(() => {
-        console.log(formData);
+        if (currentStep === pages.length - 1) {
+            submitForm(formData);
+        }
     });
 
     function collectData(data) {
-        setFormData({...formData, ...data});
+        setFormData({ ...formData, ...data });
         nextStep();
     }
 
@@ -28,6 +32,12 @@ function SubscribeForm({ submitForm }) {
 
     return (
         <>
+            <Stepper activeStep={currentStep}>
+                <Step><StepLabel>Login</StepLabel></Step>
+                <Step><StepLabel>Personal info</StepLabel></Step>
+                <Step><StepLabel>Address</StepLabel></Step>
+                <Step><StepLabel>Final</StepLabel></Step>
+            </Stepper>
             {pages[currentStep]}
         </>
     );
