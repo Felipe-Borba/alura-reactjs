9de59b7aa04472a34be941373b15b27d437dcd62
Felipe Borba <felipe.borba0109@gmail.com>
2021-06-08 18:35:25 -0300

    04-02 display post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
 import Page404 from './paginas/Page404'
 import NavBar from './components/NavBar'
+import Post from './paginas/Post';
 
 function App() {
 
@@ -13,15 +14,23 @@ function App() {
     <Router>
       <NavBar />
       <Switch>
+
         <Route exact path='/'>
           <Home />
         </Route>
+
         <Route path='/sobre'>
           <Sobre />
         </Route>
+
+        <Route path='/posts/:id'>
+          <Post />
+        </Route>
+
         <Route>
           <Page404 />
         </Route>
+
       </Switch>
     </Router>
   )
--- src/components/PosList.jsx
+++ src/components/PosList.jsx
@@ -4,33 +4,20 @@ import { search } from "../api/api";
 
 const PosList = ({ url }) => {
 
-    const [posts, setPosts] = useState([{
-        "id": 1,
-        "title": "Banho no cão",
-        "metadescription": "Use as utensilios certos para banhar seu cão",
-        "body": "A maioria dos cães prefere pular a hora do banho, mas o banho desempenha um papel importante na saúde do pelo e da pele do seu cão, ajudando a mantê-lo limpo e livre de sujeira e parasitas. Ter os produtos certos pode ajudá-lo a se sentir mais confortável.",
-        "categoria": "bem-estar",
-        "subcategoria": "higiene"
-      },
-      {
-        "id": 2,
-        "title": "Seu cachorro merece um tapete higiênico",
-        "metadescription": "O banheiro que seu cachorro merece",
-        "body": "Os tapetes higiênicos são uma inovação do mercado pet, perfeitos para que seu cão faça suas necessidades com conforto e sem sujar a casa. São produtos descartáveis ou laváveis que garantem conforto ao animal.",
-        "categoria": "bem-estar",
-        "subcategoria": "higiene"
-      }])
+    const [posts, setPosts] = useState([])
 
     useEffect(() => {
         search(url, setPosts)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, [url])
 
     return (
         <section className='posts container' >
             {
                 posts.map((post) => (
-                    <Link className={`cartao-post cartao-post--${post.categoria}`}>
+                    <Link
+                        className={`cartao-post cartao-post--${post.categoria}`}
+                        to={`/posts/${post.id}`}
+                    >
                         <article key={post.id}>
                             <h3 className='cartao-post__titulo'>
                                 {post.title}
