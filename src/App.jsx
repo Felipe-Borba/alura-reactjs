981c78678b496cd5f0d6d912f837d165f7c2157a
Felipe Borba <felipe.borba0109@gmail.com>
2021-06-08 16:28:01 -0300

    02-05 NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './assets/css/base/base.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
 import Page404 from './paginas/Page404'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <Router>
+      <NavBar />
       <Switch>
         <Route exact path='/'>
           <Home />
