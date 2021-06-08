ed4791f4fe97539a96893cadb158c60e406d98af
Felipe Borba <felipe.borba0109@gmail.com>
2021-06-08 16:37:36 -0300

    03-02 Link

--- src/paginas/Sobre.jsx
+++ src/paginas/Sobre.jsx
@@ -14,7 +14,7 @@ const Sobre = () => {
         <article className="cartao">
           O blog do doguito foi criado para tirar diversas dúvidas sobre o mundo pet!!!
         </article>
-        
+
       </section>
     </main>
   )
