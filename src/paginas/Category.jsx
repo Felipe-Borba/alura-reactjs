111c684589ee283b68ac134a659f370766e8d0ee
Felipe Borba <felipe.borba0109@gmail.com>
2021-06-08 23:43:57 -0300

    05-06 display subCategory

--- src/paginas/Category.jsx
+++ src/paginas/Category.jsx
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from 'react'
 import CategoryList from '../components/CategoryList'
 import PosList from '../components/PosList'
 import { Route, useParams, useRouteMatch } from 'react-router'
-import '../assets/css/blog.css'
 import { search } from '../api/api'
+import { Link } from 'react-router-dom'
+import SubCategory from './SubCategory'
+import '../assets/css/blog.css'
 
 const Category = () => {
     const { id } = useParams()
-    const { path } = useRouteMatch()
+    const { url, path } = useRouteMatch()
     const [subCategories, setSubCategories] = useState([])
 
     useEffect(() => {
         search(`/categorias/${id}`, (category) => {
-            setSubCategories(category.subCategories)
+            setSubCategories(category.subcategorias)
         })
     }, [id])
 
@@ -24,9 +26,30 @@ const Category = () => {
 
             <CategoryList />
 
-            <Route exact path={`${path}/`}>
-                <PosList url={`/posts?categoria=${id}`} />
-            </Route>
+            <ul className="lista-categorias container flex">
+                {
+                    subCategories.map((subcategory) => (
+                        <li
+                            className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+                            key={subcategory}
+                        >
+                            <Link to={`${url}/${subcategory}`}>
+                                {subcategory}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+
+            <switch>               
+                <Route exact path={`${path}/`}>
+                    <PosList url={`/posts?categoria=${id}`} />
+                </Route>
+
+                <Route path={`${path}/:subCategory`}>
+                    <SubCategory />
+                </Route>
+            </switch>
         </>
     )
 }
