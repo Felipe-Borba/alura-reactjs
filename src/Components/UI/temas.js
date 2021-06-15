d2be15f5a3ec596cb29b9360e77d04bcdb0daa50
Felipe Borba <felipe.borba0109@gmail.com>
2021-06-15 15:14:50 -0300

    05-02 add new theme

---
+++ src/Components/UI/temas.js
@@ -0,0 +1,20 @@
+import {
+  fundoClaro,
+  ConteudoClaro,
+  textoFundoClaro,
+  fundoEscuro,
+  ConteudoEscuro,
+  textoFundoEscuro,
+} from "./variaveis";
+
+export const temaClaro = {
+    body: fundoClaro,
+    inside: ConteudoClaro,
+    text: textoFundoClaro,
+};
+
+export const temaEscuro = {
+    body: fundoEscuro,
+    inside: ConteudoEscuro,
+    text: textoFundoEscuro,
+};
