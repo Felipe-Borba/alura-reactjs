2a1da92b154b08487a2aadaac94d1a7d1464f1aa
Felipe Borba <felipe.borba0109@gmail.com>
2021-06-15 14:36:00 -0300

    04-05 icons

--- src/Components/Items/index.js
+++ src/Components/Items/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
-import Item from '../Item';
+import React from "react";
+import styled from "styled-components";
+import Item from "../Item";
+import ImageFilter from "../ImageFilter";
 
 const Items = styled.div`
-
-box-shadow: 4px 4px 20px 0px rgba(0,0,0,0.1);
-border-radius: 10px;
-margin: 2px 0;
-display: flex;
-align-items: center;
-justify-content: space-around;
-padding: 10px;
-font-size: 12px;
+  box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  margin: 2px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 10px;
+  font-size: 12px;
 `;
 
 export default (props) => {
-    return (
-        <Items>
-            <Item {...props.data} />
-            <span>{props.data.date}</span>
-        </Items>
-    )
+  return (
+    <Items>
+      {ImageFilter(props.data.type)}
+      <Item {...props.data} />
+      <span>{props.data.date}</span>
+    </Items>
+  );
 };
